e10e6fb85c33f0da865304f522c2739c2be65fe4
Mark Amoah <mamoah@medsov.com>
2025-06-18 13:56:54 +0000

    Refactor inpatient finder and table components for fetching and displaying inpatient data from database

--- interface/modules/custom_modules/oe-inpatient-module/public/components/organisms/msv-inpatient-finder-content.php
+++ interface/modules/custom_modules/oe-inpatient-module/public/components/organisms/msv-inpatient-finder-content.php
@@ -1,3 +1,280 @@
+<?php
+
+
+/*
+ *
+ * @package     OpenEMR Inpatient Module
+ * @link        https://lifemesh.ai/telehealth/
+ *
+ * @author      Kenneth <kenneth@example.com>
+ * @author      Mark Amoah <mcprah@gmail.com>
+ * @copyright   Copyright (c) 2022 MedSov <info@medsov.com>
+ * @license     GNU General Public License 3
+ *
+ */
+
+use OpenEMR\Modules\InpatientModule\AdmissionQueueQuery;
+use OpenEMR\Modules\InpatientModule\ActionPlanQuery;
+use OpenEMR\Modules\InpatientModule\AuthQuery;
+use OpenEMR\Modules\InpatientModule\BedQuery;
+use OpenEMR\Modules\InpatientModule\FoodQuery;
+use OpenEMR\Modules\InpatientModule\InpatientQuery;
+use OpenEMR\Modules\InpatientModule\ListsQuery;
+use OpenEMR\Modules\InpatientModule\TreatmentQuery;
+use OpenEMR\Modules\InpatientModule\NoteQuery;
+use OpenEMR\Modules\InpatientModule\ReferralQuery;
+use OpenEMR\Modules\InpatientModule\VitalQuery;
+use OpenEMR\Modules\InpatientModule\WardQuery;
+use OpenEMR\Modules\InpatientModule\WardTransferQuery;
+use OpenEMR\Modules\InpatientModule\ProcedureQuery;
+use OpenEMR\Modules\InpatientModule\TheaterQuery;
+use OpenEMR\Modules\InpatientModule\SurgeryQuery;
+use OpenEMR\Modules\InpatientModule\PreDischargeChecklistQuery;
+
+use Ramsey\Uuid\Uuid;
+
+require_once __DIR__ . "/../../../../../../globals.php";
+
+require_once __DIR__ . "/../sql/ActionPlanQuery.php";
+require_once __DIR__ . "/../sql/AdmissionQueueQuery.php";
+require_once __DIR__ . "/../sql/AuthQuery.php";
+require_once __DIR__ . "/../sql/FoodQuery.php";
+require_once __DIR__ . "/../sql/BedQuery.php";
+require_once __DIR__ . "/../sql/InpatientQuery.php";
+require_once __DIR__ . "/../sql/ListsQuery.php";
+require_once __DIR__ . "/../sql/TreatmentQuery.php";
+require_once __DIR__ . "/../sql/NoteQuery.php";
+require_once __DIR__ . "/../sql/ReferralQuery.php";
+require_once __DIR__ . "/../sql/VitalQuery.php";
+require_once __DIR__ . "/../sql/WardQuery.php";
+require_once __DIR__ . "/../sql/WardTransferQuery.php";
+require_once __DIR__ . "/../sql/ProcedureQuery.php";
+require_once __DIR__ . "/../sql/TheaterQuery.php";
+require_once __DIR__ . "/../sql/SurgeryQuery.php";
+
+// require_once "./components/sql/PreDischargeChecklistQuery.php";
+
+
+$inpatients = [];
+$actionPlanQuery = new ActionPlanQuery();
+$admissionQueuQuery = new AdmissionQueueQuery();
+$authQuery = new AuthQuery();
+$bedQuery = new BedQuery();
+$inpatientQuery = new InpatientQuery();
+$listQuery = new ListsQuery();
+$treatmentQuery = new TreatmentQuery();
+$noteQuery = new NoteQuery();
+$referralQuery = new ReferralQuery();
+$vitalQuery = new VitalQuery();
+$wardQuery = new WardQuery();
+$transferQuery = new WardTransferQuery();
+$procedureQuery = new ProcedureQuery();
+$theaterQuery = new TheaterQuery();
+$surgeryQuery = new SurgeryQuery();
+$foodQuery = new FoodQuery();
+// $preDischargeChecklist = new PreDischargeChecklistQuery();
+
+
+
+$wards = $wardQuery->getWards();
+$issueList = $actionPlanQuery->getIssueList();
+$users = $authQuery->getUsers();
+$providers = $authQuery->getProviders();
+$procedure = $procedureQuery->getProcedure();
+$theater = $theaterQuery->getTheater();
+$foodItems = $foodQuery->getMenuItems();
+$calenderCat =  $surgeryQuery->getCalendarCat();
+// $checklistItems = $listQuery->getListItemsByListId('pre_discharge_items');
+
+
+$sidebar_status =  'none';
+$current_list = '';
+
+$display_mesasge = 'none';
+$message = '';
+if (isset($_GET['status'])) {
+    $message = $_GET['message'];
+    $display_mesasge = 'block';
+}
+
+if (isset($_GET['search_by_ward']) && $_SERVER['REQUEST_METHOD'] == "GET") {
+    $id = $_GET['search_ward'];
+    $word = $_GET['word'];
+    $data = [
+        'ward_id' => $id,
+        'word' =>  $word
+    ];
+    $inpatients = $inpatientQuery->searchInpatients($data);
+} else {
+    $inpatients = $inpatientQuery->getInpatients();
+}
+
+// if (isset($_POST['nurse_note']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'admission_id' => $_POST['note_admission_id'],
+//         'patient_id' => $_POST['note_pid'],
+//         'note' => $_POST['note'],
+//     ];
+//     $noteQuery->insertNote($data);
+//     header('location:inpatient.php?status=success&message=Nurse note added successfully');
+// } elseif (isset($_POST['treatment_plan']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $uuid = Uuid::uuid4();
+
+//     $data = [
+//         'admission_id' => $_POST['mng_admission_id'],
+//         'patient_id' => $_POST['mng_patient_id'],
+//         'title' => $_POST['title'],
+//         'type' => $_POST['type'],
+//         'instructions' => $_POST['instructions'],
+//         'action_start_time' => $_POST['action_start_time'],
+//         'action_end_time' => $_POST['action_end_time'],
+//         'time_interval' => $_POST['time_interval'],
+//         'staff_id' => $_POST['staff_id'],
+//         'uuid' => $uuid,
+//     ];
+//     $treatmentQuery->insertTreatmentPlan($data);
+//     $listQuery->insertList($data);
+//     header('location:inpatient.php?status=success&message=Clinical Management Plan added successfully');
+// } elseif (isset($_POST['food_req_new']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'patient' => $_POST['pid'],
+//         'food' => $_POST['food'],
+//         'staff' => $_POST['staff'],
+//         'requested_date' => $_POST['requested_date'],
+//         'admission_id' => $_POST['admission_id']
+//     ];
+//     $foodQuery->insertFoodRequest($data);
+//     header('location:inpatient.php?status=success&message=Meal Requested successfully');
+// } elseif (isset($_POST['tracker']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'plan_id' => $_POST['plan_id'],
+//         'admission_id' => $_POST['admission_id'],
+//         'action_time' => $_POST['action_time'],
+//         'staff_id' => $_POST['staff_id'],
+//     ];
+//     $treatmentQuery->insertTracker($data);
+//     header('location:inpatient.php?status=success&message=Clinical Management Plan Tracker added successfully');
+// } elseif (isset($_POST['transfer_ward']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'admission_id' => $_POST['admission_id'],
+//         'admission_date' => $_POST['admission_date'],
+//         'patient_id' => $_POST['patient_id'],
+//         'old_ward' => $_POST['old_ward'],
+//         'old_bed' => $_POST['old_bed'],
+//         'ward_id' => $_POST['ward'],
+//         'bed_id' => $_POST['bed'],
+//         'transfer_date' => $_POST['transfer_date'],
+//     ];
+//     $transferQuery->transferWard($data);
+//     header('location:inpatient.php?status=success&message=Patient ward transfer successfully');
+// } elseif (isset($_POST['referral']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'billing_facility_id' => $_POST['billing_facility_id'],
+//         'body' => $_POST['body'],
+//         'refer_date' => $_POST['refer_date'],
+//         'refer_diag' => $_POST['refer_diag'],
+//         'refer_from' => $_POST['refer_from'],
+//         'refer_external' => $_POST['refer_external'],
+//         'refer_risk_level' => $_POST['refer_risk_level'],
+//         'refer_to' => $_POST['refer_to'],
+//         'pid' => $_POST['pid'],
+//         // 'refer_vitals' => $_POST['refer_vitals'],
+//     ];
+//     // print_r(['id' => $_POST['ref_admission_id'], 'bed_id' => $_POST['ref_bed_id']]);
+//     $referralQuery->insertReferral($data);
+//     $inpatientQuery->dischargeInpatient([
+//         'id' => $_POST['ref_admission_id'],
+//         'bed_id' => $_POST['ref_bed_id'],
+//     ]);
+//     header('location:inpatient.php?status=success&message=Patient Referred successfully');
+// } elseif (isset($_POST['inpatient_vitals']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'admission_id' => $_POST['vit_admission_id'],
+//         'patient_id' => $_POST['vit_pid'],
+//         'blood_pressure' => $_POST['blood_pressure'],
+//         'pulse' => $_POST['pulse'],
+//         'temperature' => $_POST['temperature'],
+//         'respiratory_rate' => $_POST['respiratory_rate'],
+//         'spo_2' => $_POST['spo_2'],
+//         'height' => $_POST['height'],
+//         'weight' => $_POST['weight'],
+//         'fluid_input' => $_POST['fluid_input'],
+//         'fluid_output' => $_POST['fluid_output'],
+//         'staff_id' => $_POST['staff_id'],
+//         'time_taken' => $_POST['time_taken'],
+//     ];
+//     $vitalQuery->insertVital($data);
+//     header('location:inpatient.php?status=success&message=Vitals added successfully');
+// } elseif (isset($_POST['discharge']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'id' => $_POST['dis_admission_id'],
+//         'bed_id' => $_POST['dis_bed_id'],
+//         'deceased' => $_POST['deceased'],
+//         'deceased_date' => $_POST['deceased_date'],
+//         'deceased_reason' => $_POST['deceased_reason'],
+//     ];
+//     $inpatientQuery->dischargeInpatient($data);
+//     header('location:inpatient.php?status=success&message=Patient discharged successfully');
+// } elseif (isset($_POST['prep_discharge']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'id' => $_POST['prep_admission_id'],
+//         'patient_id' => $_POST['prep_patient_id'],
+//         'encounter_id' => $_POST['prep_encounter_id'],
+//         'checklistItems' => $_POST['checklist_items'] ?? [],
+//         'checklistNotes' => $_POST['checklist_notes'] ?? [],
+//     ];
+
+//     // Insert the new predischarge form entry
+//     $preDischargeChecklist->insertForm($data['id'], $data['checklistItems'], $data['checklistNotes']);
+//     $inpatientQuery->prepareDischarge($data);
+
+//     header('location:inpatient.php?status=success&message=Patient discharge preparation successfully');
+// } elseif (isset($_POST['undo_admission']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'id' => $_POST['undo_admission_id'],
+//         'bed_id' => $_POST['undo_bed_id'],
+//     ];
+//     $inpatientQuery->undoAdmission($data);
+//     header('location:inpatient.php?status=success&message=Admission Undone');
+// } elseif (isset($_POST['new_surgery']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+//     $data = [
+//         'code' => $_POST['code'],
+//         'patient_id' => $_POST['patient_id'],
+//         'procedure_id' => $_POST['procedure_id'],
+//         'theater_id' => $_POST['theater_id'],
+//         'admission_id' => $_POST['admission_id'],
+//         'start_date' => $_POST['start_date'],
+//         'end_date' => $_POST['end_date'],
+//         'status' => $_POST['status'],
+//         'created_by' => $_POST['created_by'],
+
+//         'pc_title' => $_POST['pc_title'],
+//         'pc_hometext' => $_POST['pc_hometext'],
+
+//         'duration' => $_POST['duration'],
+//         'pc_alldayevent' => $_POST['pc_alldayevent'] == 'on' ? 1 : 0,
+
+//     ];
+
+//     $surgeryQuery->insertSurgery($data);
+//     header('location:inpatient.php?status=success&message=Patient Booked for Surgery successfully');
+// }
+
+// if (isset($_GET['show_treatment_plans']) && $_SERVER['REQUEST_METHOD'] == "GET") {
+//     $sidebar_status = 'display';
+//     $current_list = 'treatment_plansx';
+//     $patientManagement = $treatmentQuery->getPatientTreatmentPlan($_GET['admission_id']);
+// } elseif (isset($_GET['show_vitals']) && $_SERVER['REQUEST_METHOD'] == "GET") {
+//     $sidebar_status = 'display';
+//     $current_list = 'vitals';
+//     $patientVitals = $vitalQuery->getAdmissionVitals($_GET['admission_id']);
+// } elseif (isset($_GET['show_notes']) && $_SERVER['REQUEST_METHOD'] == "GET") {
+//     $sidebar_status = 'display';
+//     $current_list = 'notes';
+//     $patientNotes = $noteQuery->getAdmissionNote($_GET['admission_id']);
+// }
+
+?>
 <main class="flex-1">
     <div class="bg-gradient-to-b h-[241px] from-[#FFA97F] to-[#ED2024] p-6">
         <p class="text-[32px] font-[500] text-white ml-12">Patient Finder</p>
--- interface/modules/custom_modules/oe-inpatient-module/public/components/organisms/tables/finder-table.php
+++ interface/modules/custom_modules/oe-inpatient-module/public/components/organisms/tables/finder-table.php
@@ -1,55 +1,92 @@
 <div>
     <?php
-        $columns = [
-            [
-                'title' => 'Patient ID',
-                'dataIndex' => 'id'
-            ],
-            [
-                'title' => 'Full Name',
-                'dataIndex' => 'name'
-            ],
-            [
-                'title' => 'Ward',
-                'dataIndex' => 'ward'
-            ],
-            [
-                'title' => 'Bed',
-                'dataIndex' => 'bed'
-            ],
-            [
-                'title' => 'Admitted',
-                'dataIndex' => 'addmitted'
-            ],
-            [
-                'title' => 'Days Spent',
-                'dataIndex' => 'days_spent'
-            ],
-            [
-                'title' => 'Actions',
-                'dataIndex' => 'actions',
-                'render' => function($record) {
-                    $patientId = htmlspecialchars($record['id']);
-                    // This function allows you to render custom HTML for a cell.
-                    // $record contains the entire row's data.
-                    return '<div class="flex space-x-2">
-                                <a role="button" href="dashboard.php?id=' .$patientId. '" class="bg-blue-500 hover:bg-blue-600 text-white text-xs px-2 py-1 rounded-md shadow-sm transition duration-200">View</a>
+
+    $columns = [
+        [
+            'title' => 'Patient ID',
+            'dataIndex' => 'id'
+        ],
+        [
+            'title' => 'Full Name',
+            'dataIndex' => 'name'
+        ],
+        [
+            'title' => 'Ward',
+            'dataIndex' => 'ward'
+        ],
+        [
+            'title' => 'Bed',
+            'dataIndex' => 'bed'
+        ],
+        [
+            'title' => 'Admitted',
+            'dataIndex' => 'addmitted'
+        ],
+        [
+            'title' => 'Days Spent',
+            'dataIndex' => 'days_spent'
+        ],
+        [
+            'title' => 'Actions',
+            'dataIndex' => 'actions',
+            'render' => function ($record) {
+                $patientId = htmlspecialchars($record['id']);
+                return '<div class="flex space-x-2">
+                                <a role="button" href="dashboard.php?id=' . $patientId . '" class="bg-blue-500 hover:bg-blue-600 text-white text-xs px-2 py-1 rounded-md shadow-sm transition duration-200">View</a>
                                 <button class="bg-red-500 hover:bg-red-600 text-white text-xs px-2 py-1 rounded-md shadow-sm transition duration-200">Delete</button>
                             </div>';
-                }
-            ],
-        ];
-        $dataSource = [
-            ['id' => 'P001', 'name' => 'Lily Cho', 'ward' => "Woman’s Medical", 'bed' => '001', 'addmitted' => '2025-06-04 14:59:05',"days_spent" => 12],
-            ['id' => 'P002', 'name' => 'Jane Smith', 'ward' => "Woman’s Medical", 'bed' => '002', 'addmitted' => '2025-06-04 14:59:05',"days_spent" => 12],
-            ['id' => 'P003', 'name' => 'Mike Johnson', 'ward' => "Woman’s Medical", 'bed' => '003', 'addmitted' => '2025-06-04 14:59:05', "days_spent" => 12],
-            ['id' => 'P004', 'name' => 'Emily Brown', 'ward' => "Woman’s Medical", 'bed' => '004', 'addmitted' => '2025-06-04 14:59:05', "days_spent" => 12],
-            ['id' => 'P005', 'name' => 'Chris Green', 'ward' => "Woman’s Medical", 'bed' => '005', 'addmitted' => '2025-06-04 14:59:05', "days_spent" => 12],
-        ];
+            }
+        ],
+    ];
 
-        $isLoading = false;
-        $responsive = true;
+    $dataSource = [];
+    // Map $inpatients to $dataSource for the table
+    // If $inpatients is an object (e.g., sqlStatement result), fetch rows as arrays
+    
+    if (is_object($inpatients)) {
+        while ($item = sqlFetchArray($inpatients)) {
+            $dataSource[] = [
+                'id' => $item['patient_id'] ?? $item['id'] ?? '',
+                'name' => trim(($item['fname'] ?? '') . ' ' . ($item['mname'] ?? '') . ' ' . ($item['lname'] ?? '')),
+                'ward' => $item['ward_name'] ?? '',
+                'bed' => $item['bed_number'] ?? '',
+                'addmitted' => $item['admission_date'] ?? '',
+                'days_spent' => (isset($item['admission_date']) && $item['admission_date'])
+                    ? (
+                        isset($item['discharge_date']) && $item['discharge_date']
+                        ? max(1, (int) ceil((strtotime($item['discharge_date']) - strtotime($item['admission_date'])) / 86400))
+                        : max(1, (int) ceil((time() - strtotime($item['admission_date'])) / 86400))
+                    )
+                    : ''
+            ];
+        }
+    } elseif (is_array($inpatients)) {
+        foreach ($inpatients as $item) {
+            if (is_object($item)) {
+                $item = (array)$item;
+            }
+            if (is_array($item)) {
+                $dataSource[] = [
+                    'id' => $item['patient_id'] ?? $item['id'] ?? '',
+                    'name' => trim(($item['fname'] ?? '') . ' ' . ($item['mname'] ?? '') . ' ' . ($item['lname'] ?? '')),
+                    'ward' => $item['ward_name'] ?? '',
+                    'bed' => $item['bed_number'] ?? '',
+                    'addmitted' => $item['admission_date'] ?? '',
+                    'days_spent' => (isset($item['admission_date']) && $item['admission_date'])
+                        ? (
+                            isset($item['discharge_date']) && $item['discharge_date']
+                            ? max(1, (int) ceil((strtotime($item['discharge_date']) - strtotime($item['admission_date'])) / 86400))
+                            : max(1, (int) ceil((time() - strtotime($item['admission_date'])) / 86400))
+                        )
+                        : ''
+                ];
+            }
+        }
+    }
 
-        include_once __DIR__ . '/data-table.php';
-        ?>
+    $isLoading = false;
+    $responsive = true;
+
+    include_once __DIR__ . '/data-table.php';
+    ?>
 </div>
